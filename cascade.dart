9ee57d72170f943230bdaa764280f1b935d322f3
jqkk <kkj2551@naver.com>
2024-01-14 22:32:02 +0900

    feat: cascade

--- cascade.dart
+++ cascade.dart
@@ -1,9 +1,11 @@
 import 'classes/Meal.dart';
 
 void main() {
-  Meal(['김치찌개', '된장찌개', '제육볶음'])
+  final meal = Meal(['김치찌개', '된장찌개', '제육볶음'])
     ..printDishes()
     ..useSpoon()
     ..useChopsticks()
     ..useSpoon();
+
+  print(meal);
 }
